30aa7f078733aa5c68c33cebe6c6be4ad61bf392
MAldeep <m7mdeldeeeep@gmail.com>
2025-04-27 06:08:08 +0300

    version 27-4-2025

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-(function(){
+// updated version
+// this part is added as it's a form so i think submitting the form reloads the page as a default setting..
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+});
+// important..
+let showResult = () =>{
     const inches = document.querySelector('#value');
     const result = document.querySelector('p');
-    const form = document.querySelector('form');
-    form.addEventListener('submit', function(event){
-        event.preventDefault();
-        const distance = parseFloat(inches.value);
-        if(distance){
-            let convertedResult = (distance / 39.37).toFixed(2);            
-            result.innerHTML = `${distance} inches equals ${convertedResult} metres.`;
-        }else {
-            result.innerHTML = "Please insert a number !!";
-        }
-    });
-})();
+    const distance = parseFloat(inches.value);
+    let convertedResult = (distance / 39.37).toFixed(1);
+    result.innerHTML = `${distance} inches equals ${convertedResult} metres.`;
+}
+
+
+
+
+//i think it could be done in easier way but it's working for now at least..
+// دي كانت القديمة من التاسك الاول
+// (function(){
+//     const inches = document.querySelector('#value');
+//     const result = document.querySelector('p');
+//     const form = document.querySelector('form');
+//     form.addEventListener('submit', function(event){
+//         event.preventDefault();
+//         const distance = parseFloat(inches.value);
+//         if(distance){
+//             let convertedResult = (distance / 39.37).toFixed(2);            
+//             result.innerHTML = `${distance} inches equals ${convertedResult} metres.`;
+//         }else {
+//             result.innerHTML = "Please insert a number !!";
+//         }
+//     });
+// })();
 
